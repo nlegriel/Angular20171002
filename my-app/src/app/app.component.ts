95c62b455751328a1acdec81903c3facc9d086dc
nlegriel <dev.nlegriel@orange.fr>
2017-10-03 10:25:24 +0200

    Boucle pour application de livraison

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  collection: any;
+
+  ngOnInit(): void {
+    this.collection = [
+      {reference: '1234', name: 'Marina'},
+      {reference: '2314', name: 'Patrick'},
+      {reference: '2277', name: 'Quentin'}
+    ];
+  }
 }
